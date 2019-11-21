6c94094b3dc0b540f56baa984cfe430aa22198c6
Aaron Botello <aaron.botello.h@gmail.com>
2019-11-20 21:06:53 -0600

    js d

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
 
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
